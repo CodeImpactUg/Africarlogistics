7a0d2b7b6209591619818fd30f3a6dc76c303dc9
Sajj96 <captainsajjad4@gmail.com>
2019-12-06 20:36:57 +0300

    Initial commit

--- src/views/App.js
+++ src/views/App.js
@@ -3,6 +3,7 @@ import AppLoader from "../components/AppLoader";
 import AppHeader from "../components/AppHeader";
 import AppHeaderSlider from "../components/AppHeaderSlider";
 import TravelPlaces from "../components/TravelPlaces";
+import ClientsReviews from "../components/ClientsReviews";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <AppHeader />
         <AppHeaderSlider />
         <TravelPlaces />
+        <ClientsReviews />
       </Fragment>
     );
   }
