8ed56c9f0302582e2094aab010bd58514ae4cfbe
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2020-02-13 15:47:34 +0300

    install react router dom

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,14 @@
 import React, { Component, Fragment } from "react";
-import AppLoader from "../AppLoader";
-import AppHeader from "../AppHeader";
 import AppHeaderSlider from "../AppHeaderSlider";
 import TravelPlaces from "./TravelPlaces";
-import ClientReviews from "./ClientReviews";
-import Gallery from "./Gallery";
-
+import ClientsReviews from "./ClientReviews";
 class Home extends Component {
   render() {
     return (
       <Fragment>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <AppHeaderSlider />
         <TravelPlaces />
         <ClientsReviews />
-=======
-=======
->>>>>>> c73ef342483bf8f9a81d00ff24bf3e55e29c09a7
-        <AppLoader />
-        <AppHeader />
-        <AppHeaderSlider />
-        <TravelPlaces />
-        <ClientReviews />
-        <Gallery />
-<<<<<<< HEAD
->>>>>>> feature gallery
-=======
->>>>>>> c73ef342483bf8f9a81d00ff24bf3e55e29c09a7
       </Fragment>
     );
   }
