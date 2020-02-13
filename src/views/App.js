1530c80096011b15cd60840c9066b98904a80645
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2020-02-13 15:19:54 +0300

    resolve conflicts

--- src/views/App.js
+++ src/views/App.js
@@ -1,38 +1,21 @@
-<<<<<<< HEAD
 import React, { Component, Fragment } from "react";
 import AppLoader from "../components/AppLoader";
 import AppHeader from "../components/AppHeader";
-import AppHeaderSlider from "../components/AppHeaderSlider";
-import TravelPlaces from "../components/TravelPlaces";
-import ClientsReviews from "../components/ClientsReviews";
 import Footer from "../components/Footer/Footer";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../components/Home/Home";
 
-=======
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "../components/Home/Home";
->>>>>>> feature gallery
-
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-
       <BrowserRouter>
         <Switch>
-        <Fragment>
-        <AppLoader />
-        <AppHeader />
-        <Route path="/" exact component={Home} />
-        <Footer />
-        </Fragment>
-=======
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
->>>>>>> feature gallery
+          <Fragment>
+            <AppLoader />
+            <AppHeader />
+            <Route path="/" exact component={Home} />
+            <Footer />
+          </Fragment>
         </Switch>
       </BrowserRouter>
     );
